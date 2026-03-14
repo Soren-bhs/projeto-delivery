22f13715fa4b66f116d2ba0942f13039117a90a8
Soren-bhs <bruno.bhs.2006@gmail.com>
2026-03-14 15:16:59 -0300

    style: resposividade para celular

--- src/pages/client/Catalog.tsx
+++ src/pages/client/Catalog.tsx
@@ -19,14 +19,20 @@ export function Catalog() {
     }, [isDark]);
 
     return (
-        <div className='flex justify-center'>
-            <div className='w-2/6 flex justify-center items-center p-5 relative'>
-                <Search size={25} className='absolute left-10 text-zinc-400' />
-                <input
-                    className='ml-1 w-1/1 rounded-2xl p-4 pl-12 font-bold bg-white text-zinc-400 border dark:bg-slate-900 transition-colors duration-300'
-                    placeholder='Pesquise sua comida...'
-                ></input>
+        <div className='flex justify-center items-center g-4'>
+            <div className='lg:w-2/6 w-1/1 flex flex-col justify-center p-5 relative'>
+                <div className='w-full flex justify-center items-center'>
+                    <Search size={25} className='absolute left-10 text-zinc-400' />
+                    <input
+                        className='ml-1 w-1/1 rounded-2xl border-zinc-300 p-4 pl-12 font-bold bg-white text-zinc-400 border dark:bg-slate-900 transition-colors duration-300'
+                        placeholder='Pesquise sua comida...'
+                    ></input>
+                </div>
+                <div className='text-2xl font-bold py-4'>
+                    <h2 className='text-zinc-600'>Categorias</h2>
+                </div>
             </div>
+            
         </div>
     )
 }
